448cf942eda2943e875693f4a26ba5f86ac67177
sepehrdaad <63661177+sepehrdaad@users.noreply.github.com>
2020-05-26 16:58:14 +0300

    Update zscore_seeg.m

--- zscore_seeg.m
+++ zscore_seeg.m
@@ -12,10 +12,10 @@ function data = zscore_seeg(data, timebase)
 samples_base = samples_base1:samples_base2;
 
 for tr = 1:length(data.trial)
-    for ch = 1:length(data.label)
+      for ch = 1:length(data.label)
+          [tr ch]
         data.zscore(tr,ch)  = std(data.trial{1,tr}(ch,samples_base));
-        data.trial{1,tr} = data.trial{1,tr}(ch,:)/data.zscore(tr);
+        data.trial{1,tr}(ch,:) = data.trial{1,tr}(ch,:)/data.zscore(tr);
     end    
 end
 
- 
