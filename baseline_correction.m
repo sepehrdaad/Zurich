ac246aaa1b47b5b571ab318608ccdd20a07feccd
tommytommy81 <tommy81.handy@gmail.com>
2020-05-26 14:39:52 +0300

    baseline bug also corrected

--- baseline_correction.m
+++ baseline_correction.m
@@ -1,18 +1,21 @@
-function data = baseline_correction(data, timebase)   
+function data = baseline_correction(data, timebase)
 
 % function to reduce number of channels in the ft struct
 % input:
 % data, timebase [seconds]
 % output
 % data zscored
- 
+
 [~,samples_base1] =  (min(abs(timebase(1)-data.time{1,1})));
 [~,samples_base2] =  (min(abs(timebase(2)-data.time{1,1})));
 
 samples_base = samples_base1:samples_base2;
 
 for tr = 1:length(data.trial)
-    data.trial{1,tr} = data.trial{1,tr}-mean(data.trial{1,tr}(samples_base));
+    for ch = 1:length(data.label)        
+        data.trial{1,tr}(ch,:) = data.trial{1,tr}(ch,:)-mean(data.trial{1,tr}(ch,samples_base));
+    end
+end
 end
 
  
